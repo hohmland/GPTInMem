2f1df47f41cdb339d28b757729b47f65120af439
hohmland <info@hohmland.com>
2023-05-23 15:52:47 -0700

    install jest and add first openai test

---
+++ __tests__/todoList.test.ts
@@ -0,0 +1,30 @@
+import { chatGPT } from '../src/openai';
+
+const prompts: string[] = [
+	`I want you to act as a to do list.  I will provide a set of tasks that include the task name, the due date, and the importance of the task.\nI will also include today's date.  I want you to tell me all the tasks that are due in the next 7 days and I want you to put them in order of importance.  Be sure to check the dates carefully.  If a task is listed whose date has already expired, please identify this task as past due.  Be very concise in your answer.  All I need back from you is the list of tasks due in the next seven days.\nI will then answer you back.  I may tell you that a task is already complete.  If I tell you a task is already complete, can you remove it from the to do list?  I may tell you to add a new task to the list or I may tell you to change something about one of the existing tasks.  So we might chat back and forth a few times to update the list.\nWhen this is finished, I'll ask for the updated list with all of your changes.  Can you generate a new to do list in the same JSON format I provided in the original list, e.g. task, due date, importance.  For the generated list, please be sure to include all of the original tasks, removing only those tasks that I idendtify have already been completed.  When you answer with the updated JSON formatted list, can you again be very concise and answer with this list only?  No extra chat or verbiage, strictly the list in JSON format.  That way I can cut and paste it into another program.`,
+	`todoList: [{"task": "Write to Jim", "due_date": "May 25, 2023", "importance": 4},{"task": "Call Jane", "due_date": "May 28, 2023", "importance": 3},{"task": "Write to Jen", "due_date": "June 18, 2023", "importance": 1}]\ntodays date: May 26, 2023`,
+	`I wrote to Jim.  And can you move the Jane call to May 29?`,
+	`Can you add: Write to Joe, May 30, low importance?`
+];
+
+test('the new todo list should have three items', async () => {
+  const response = await chatGPT(prompts);
+	let chatResponseJson = [];
+	if (response && response["data"] && response["data"]["choices"] && response["data"]["choices"][0] && response["data"]["choices"][0]["message"]) {
+		const chatReponseString = response["data"]["choices"][0]["message"]["content"];
+		const match = chatReponseString.match(/(\[.*\])/)
+		if (match) chatResponseJson = JSON.parse(match[0]);
+	}
+	expect(chatResponseJson.length).toBe(3);
+});
+
+test('blank prompts array should result in zero length response', async () => {
+  const response = await chatGPT([]);
+	let chatResponseJson = [];
+	if (response && response["data"] && response["data"]["choices"] && response["data"]["choices"][0] && response["data"]["choices"][0]["message"]) {
+		const chatReponseString = response["data"]["choices"][0]["message"]["content"];
+		const match = chatReponseString.match(/(\[.*\])/)
+		if (match) chatResponseJson = JSON.parse(match[0]);
+	}
+	expect(chatResponseJson.length).toBe(0);
+});
